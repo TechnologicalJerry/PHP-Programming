c373e97d6cfbd7f5df7419e0ccb556d8a013272a
technologicaljerry <cserajatverma@gmail.com>
2022-11-09 22:33:44 +0530

    addition

---
+++ addition.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    $text = "Addition of a and b is : ";
+    $a = 10;
+    $b = 20;
+    echo "<h1>" . $text . "</h1>";
+    echo $a + $b;
+    ?>
+</body>
+
+</html>

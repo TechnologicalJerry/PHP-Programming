1aba246a0ced722c1cdb5650adadd8511032d1ad
technologicaljerry <cserajatverma@gmail.com>
2022-12-12 23:50:40 +0530

    Implement header

---
+++ header.php
@@ -0,0 +1,7 @@
+<?php  
+    // This will redirect the user to the new location  
+    header('Location: https://www.php.net/');  
+      
+    //The below code will not execute after header  
+    exit;  
+?>

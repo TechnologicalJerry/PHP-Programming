7142519fd5afd63f83f032d5ee0d09b28afcff3c
technologicaljerry <cserajatverma@gmail.com>
2022-12-25 22:11:32 +0530

    Create christmasTree.php

---
+++ christmasTree.php
@@ -0,0 +1,30 @@
+<?php
+function triangle($n)
+{
+
+    $k = 2 * $n - 2;
+
+
+    for ($i = 0; $i < $n; $i++) {
+
+
+        for ($j = 0; $j < $k; $j++)
+            echo " ";
+
+        $k = $k - 1;
+
+
+        for ($j = 0; $j <= $i; $j++) {
+
+
+            echo "* ";
+        }
+
+
+        echo "\n";
+    }
+}
+
+
+$n = 5;
+triangle($n);
